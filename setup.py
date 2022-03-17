34e6cddfcac968d24bdb20d1166e32c4611940db
Eunoia <eunoiapolis@gmail.com>
2022-03-18 04:30:11 +0530

    autocommit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='gync',
+    version='0.1',
+    description='A useful module',
+    license='MIT',
+    author='Eunoia',
+    author_email='eunoiapolis@gmail.com',
+    url='https://gync.eunoiapolis.com/',
+    packages=['gync'],
+    install_requires=['os', 'os.path', 'sys', 'subprocess'],
+)
